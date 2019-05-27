fd946b3c4f0dc547b4276761cafccf712539dd3b
0xBAMA <jon.baker1222@gmail.com>
2019-05-27 13:23:53 -0400

    first textures working

--- Texture Demo/main.cc
+++ Texture Demo/main.cc
@@ -264,13 +264,48 @@ void generate_points()
 }
 
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "../resources/stb_image.h"
+
+
+GLuint texture; //handle for the texture
 
 // OpenGL initialization
 void init( Shader s )
 {
 
-		#define STB_IMAGE_IMPLEMENTATION
-		#include "../resources/stb_image.h"
+
+
+
+
+		glGenTextures(1, &texture); // Generate an ID
+		glBindTexture(GL_TEXTURE_2D, texture); // use the specified ID
+
+		// set the texture wrapping/filtering options (on the currently bound texture object)
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+
+
+		// load and generate the texture
+		int width, height, nrChannels;
+		unsigned char *data = stbi_load("e.png", &width, &height, &nrChannels, 0);
+		if (data)
+		{
+		    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		    glGenerateMipmap(GL_TEXTURE_2D);
+		}
+		else
+		{
+		    std::cout << "Failed to load texture" << std::endl;
+		}
+		stbi_image_free(data);
+
+
+
+
 
 		generate_points();
 
